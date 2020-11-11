85cdaebb0753b8b649e3b71894e84bc428f2c7ba
Aaditya <aaditya.singh.met16@itbhu.ac.in>
2020-11-11 19:23:45 +0530

    Base class for rules following command pattern.

--- parrot/parrot.py
+++ parrot/parrot.py
@@ -9,7 +9,7 @@ from __future__ import (
 
 class DataObj:
 
-    def __init__(self, str):
+    def __init__(self, str, *args, **kwargs):
         self.docs = None
         self.span = None
         str = self.preprocess(str)
@@ -19,9 +19,37 @@ class DataObj:
 
 
 class IdentifierBase:
-    def __init__(self):
-        pass
+    def __init__(self, model, *args, **kwargs):
+        self.model = model
 
-    def __call__(self, *args, **kwargs):
-        pass
+    def predict_proba(self):
+        raise NotImplementedError
 
+    def decision_fn(self):
+        raise NotImplementedError
+
+    def __call__(self):
+        raise NotImplementedError
+
+
+class RuleBase:
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def apply(self, x):
+        raise NotImplementedError
+
+    def __call__(self, x):
+        return self.apply(x)
+
+
+class Sequential:
+    def __init__(self, rule_list):
+        assert len(rule_list) > 0
+        assert issubclass(type(rule_list[0]), RuleBase)
+        self.rules = rule_list
+        super(Sequential, self).__init__()
+
+    def __call__(self, x):
+        for rule in self.rules:
+            x = rule(x)
